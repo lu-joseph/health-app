ff7cf771c0bac7dc97b77f76f80b108dec1cf9cc
hansoo-linux <hyoon1998@gmail.com>
2022-02-19 10:30:06 -0800

    removed navbar

--- frontend/health-app/src/App.js
+++ frontend/health-app/src/App.js
@@ -1,11 +1,9 @@
-import Navbar from './components/Navbar/Navbar';
 import Dashboard from './pages/Dashboard/Dashboard';
 import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar/>
       <Routes>
         <Route path='/' element={<Dashboard/>}/>
       </Routes>
