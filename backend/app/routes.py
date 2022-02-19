e68551a68c9ec3d63ea55c135e9e0d8cd597889f
Joseph Lu <joseph.lu1@uwaterloo.ca>
2022-02-19 12:59:40 -0500

    Added basic water requests

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,6 @@
 from app import app, db
 from app.sleep import Sleep
+from app.water import Water
 from flask import request
 import json
 
@@ -8,11 +9,21 @@ import json
 def hello():
     return "hello world"
 
+##
+# Sleep
+##
+
+
+@app.route("/api/sleep/getEntries", methods=["GET"])
+def getSleepEntries():
+    entries = Sleep.query.all()
+    return json.dumps([e.serialize() for e in entries])
+
 
 @app.route("/api/sleep/addEntry", methods=["POST"])
-def addEntry():
+def addSleepEntry():
     hours = request.form.get("hours")
-    date = request.form.get("date")
+    date = request.form.get("date")  # format must be yyyy-mm-dd
     try:
         (Sleep.add_entry(hours, date))
         return 'Entry was added', 200
@@ -20,11 +31,27 @@ def addEntry():
     except Exception as e:
         return str(e), 500
 
+##
+# Sleep
+##
 
-@app.route("/api/sleep/getEntries", methods=["GET"])
-def getSleepEntries():
-    entries = Sleep.query.all()
+
+@app.route("/api/water/getEntries", methods=["GET"])
+def getWaterEntries():
+    entries = Water.query.all()
     return json.dumps([e.serialize() for e in entries])
+
+
+@app.route("/api/water/addEntry", methods=["POST"])
+def addWaterEntry():
+    cups = request.form.get("cups")
+    date = request.form.get("date")  # format must be yyyy-mm-dd
+    try:
+        (Water.add_entry(cups, date))
+        return 'Entry was added', 200
+
+    except Exception as e:
+        return str(e), 500
 
 
 if __name__ == '__main__':
