b92b7c272e1c8b6e7f00f01b4efd987bc8fcf05a
Joseph Lu <joseph.lu1@uwaterloo.ca>
2022-02-19 13:27:26 -0500

    Added mood class

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.sleep import Sleep
 from app.water import Water
+from app.mood import Mood
 from flask import request
 import json
 
@@ -32,7 +33,7 @@ def addSleepEntry():
         return str(e), 500
 
 ##
-# Sleep
+# Water
 ##
 
 
@@ -48,6 +49,28 @@ def addWaterEntry():
     date = request.form.get("date")  # format must be yyyy-mm-dd
     try:
         (Water.add_entry(cups, date))
+        return 'Entry was added', 200
+
+    except Exception as e:
+        return str(e), 500
+
+##
+# Mood
+##
+
+
+@app.route("/api/mood/getEntries", methods=["GET"])
+def getMoodEntries():
+    entries = Mood.query.all()
+    return json.dumps([e.serialize() for e in entries])
+
+
+@app.route("/api/mood/addEntry", methods=["POST"])
+def addMoodEntry():
+    score = request.form.get("score")  # must be between 1 and 5
+    date = request.form.get("date")  # format must be yyyy-mm-dd
+    try:
+        (Mood.add_entry(score, date))
         return 'Entry was added', 200
 
     except Exception as e:
