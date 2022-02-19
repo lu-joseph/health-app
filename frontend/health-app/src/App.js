12e071cbfb962d41e667376795c4898a97ace599
hansoo-linux <hyoon1998@gmail.com>
2022-02-19 14:31:07 -0800

    Created hero page

--- frontend/health-app/src/App.js
+++ frontend/health-app/src/App.js
@@ -1,11 +1,12 @@
 import Dashboard from './pages/Dashboard/Dashboard';
 import {Routes, Route} from 'react-router-dom';
+import Hero from './pages/Hero/Hero';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/' element={<Hero/>}/>
       </Routes>
     </>
   );
